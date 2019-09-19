9604cb9317ae012f37f727a0155663923fc08dc4
Sekarmano <s.manojbe2012@gmail.com>
2019-09-19 11:25:11 +0530

    The code was updated

--- src/orh/test/clone/Example.java
+++ src/orh/test/clone/Example.java
@@ -1,9 +1,13 @@
 package orh.test.clone;
 
 import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Example {
@@ -12,6 +16,20 @@ public class Example {
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.snapdeal.com");
 		driver.findElement(By.id("inputValEnter")).sendKeys("iphonex");
+		driver.findElement(By.id("inputValEnter")).sendKeys("iphonex");
+		Robot r =new Robot();
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		List<WebElement> l = driver.findElements(By.xpath("//p[@class='product-title']"));
+		int h = l.size();
+		System.out.println(h);
+		for  (WebElement x:l) {
+			System.out.println(x.getText());
+			
+		}
+		l.get(5).click();
+		
+		}
+		
 
-}
 }
